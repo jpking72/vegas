f4f21c51844acbf8ca665d5b2df41fab52f60a72
unknown <kingjon90@gmail.com>
2015-08-26 11:57:45 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<title>Vegas Letter</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="js/site.js"></script>
+		<script src="//use.typekit.net/fyl8fbw.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+	</head>
+	<body>
+		<div class="homePage">
+			<header>
+				<div class="headerContent">
+					<div class="logo"><a href="/"><img src="images/vegasLogo.jpg" alt="Vegas Logo"></a></div>
+					<div class="navigation">
+						<ul>
+							<li><a href="#">LEARN</a></li>
+							<li><a href="#">GET THE PICKS</a></li>
+							<li><a href="#">SPECIALS</a></li>
+							<li><a href="#" class="boldLinK">MY VEGAS LETTER</a></li>
+						</ul>
+					</div>
+				</div>
+			</header>
+			<div class="main">
+				<img src="images/football2.jpg" alt="Football 2" width="100%" class="mainImage">
+				<div class="contentArea">
+					<div class="largeText">Pay less to win more!</div>
+					<div class="smallText">
+						<p>The Vegas Football Letter gets the winning picks in your hands, every week, for a fraction of the cost of other newsletters and handicappers. 
+					Register today to start getting the winner's picks for the 2015-2016 NFL and college football seasons for as low as <span class="boldLink">$59.99 for the entire season!.</span></p>
+					</div>
+					<div class="learnButton">Learn More</div>
+				</div>
+				<div class="opacityOverlay"></div>
+				<div class="downArrow"><a href="#subscribe"><img src="images/downArrow.png"></a></div>
+			</div>
+		</div>
+		<div class="about"><p>loren Ipsum Dolorum fit veritas no seclorum fotus et n loren Ipsum Dolorum fit veritas no 
+		seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus et nloren Ipsum Dolorum 
+		fit veritas no seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus et n loren Ipsum Dolorum fit veritas no seclorum fotus et n loren 
+		Ipsum Dolorum fit veritas no seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus 
+		et nloren Ipsum Dolorum fit veritas no seclorum fotus et nloren Ipsum Dolorum fit veritas no seclorum fotus et.</p>
+		</div>
+		<div class="subscribe">
+			<a id="subscribe"></a>
+			<img src="images/vegasBackground.jpg" alt="Vegas Background" width="100%">
+			<div class="contentArea">
+				<div class="largeText">Insider information from<br>across the industry</div>
+				<div class="smallText">
+					<p>Unlike other newsletters, our primary source of income is generatied by playing the very same picks we publish  each week - 
+					NOT from selling the picks for hundreds of dollars. That’s why  we offer our newsletter, with ALL of our NFL and college football
+					 picks, for <span class="boldLink">less than $6 a week!</span></p>
+				</div>
+			</div>
+			<div class="subscribeText">SUBSCRIBE NOW</div>
+			<div class="opacityOverlay"></div>
+		</div>
+
+	</body>
+</html>
---
+++ js/site.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	
+	var innerHeight = window.innerHeight;
+	var imageHeight = innerHeight - 100;
+	$(".mainImage").height(imageHeight);
+
+
+})
