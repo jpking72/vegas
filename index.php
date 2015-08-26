b44a5a378b4428586d0099edb2527a2a04d531d4
unknown <kingjon90@gmail.com>
2015-08-26 15:11:19 -0500

    First page built

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 						<p>The Vegas Football Letter gets the winning picks in your hands, every week, for a fraction of the cost of other newsletters and handicappers. 
 					Register today to start getting the winner's picks for the 2015-2016 NFL and college football seasons for as low as <span class="boldLink">$59.99 for the entire season!.</span></p>
 					</div>
-					<div class="learnButton">Learn More</div>
+					<div class="learnButton"><a href="#learnmore">Learn More</a></div>
 				</div>
 				<div class="opacityOverlay"></div>
 				<div class="downArrow"><a href="#subscribe"><img src="images/downArrow.png"></a></div>
@@ -44,7 +44,7 @@
 		</div>
 		<div class="subscribe">
 			<a id="subscribe"></a>
-			<img src="images/vegasBackground.jpg" alt="Vegas Background" width="100%">
+			<img src="images/vegasBackground2.jpg" alt="Vegas Background" width="100%">
 			<div class="contentArea">
 				<div class="largeText">Insider information from<br>across the industry</div>
 				<div class="smallText">
@@ -56,6 +56,90 @@
 			<div class="subscribeText">SUBSCRIBE NOW</div>
 			<div class="opacityOverlay"></div>
 		</div>
-
+		<ul class="subscribeList">
+			<li>
+				<a href="#">
+					<div class="subText">THIS<br>WEEK'S<br>PICKS</div>
+					<div class="subPrice">$5.99</div>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<div class="subText">FULL<br>SEASON</div>
+					<div class="subPrice">$59.99</div>
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					<div class="subText">HALF<br>SEASON</div>
+					<div class="subPrice">$29.99</div>
+				</a>
+			</li>
+			<li class="noright">
+				<a href="#">
+					<div class="subText">WEEKLY<br>SPECIAL</div>
+					<div class="subPrice">Click to View</div>
+				</a>
+			</li>
+		</ul>
+		<div style="clear:both"></div>
+		<div class="learnMore">
+			<a id="learnmore"></a>
+			<h3>LEARN MORE ABOUT US</h3>
+			<ul>
+				<li>
+					<a href="#">
+						<div class="subText">SEE A<br>SAMPLE<br>ISSUE</div>
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						<div class="subText">WHY OUR<br>METHOD<br>WORKS</div>
+					</a>
+				</li>
+				<li class="doubleDiv">
+					<a href="#">
+						<div class="subText">QUESTIONS OR COMMENTS?<br><span class="largeContact">CONTACT US</span></div>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div style="clear:both"></div>
+		<div class="football2">
+			<img src="images/football3.jpg" alt="Football 1" width="100%">
+			<div class="contentArea">
+				<div class="largeText">The Only Newsletter<br>You'll Ever Need.</div>
+				<div class="smallText">
+					<p>At the Vegas Football Letter, we compile a consensus of the top Handicappers, Insiders, and Technicians (HIT Score) 
+					to create the best value-added forecast tfor the 2015-2016 NFL and college season.  Get started today for 
+					only <span class="boldLink">$59.99 for the entire season</span>!.<br><div class="termsText">Sign up today. It's time to win.</div></p>
+				</div>
+			</div>
+			<div class="opacityOverlay"></div>
+		</div>
+		<footer>
+			<div class="leftSplit">
+				<div class="facebookDiv"><a href="#"><img src="images/facebook.png"></a></div>
+			</div>
+			<div class="rightSplit">
+				<div class="twitterDiv"><a href="#"><img src="images/twitter.png"></a></div>
+			</div>
+			<div class="footerBottom">
+				<div class="leftFooter">
+					<p>The Vegas Football Letter</p>
+					<p>P.O. Box 1234</p>
+					<p>Las Vegas, NV 12345</p>
+					<p>1.800.123.4567</p>
+				</div>
+				<div class="rightFooter">
+					<p><a href="#learnmore">Learn</a></p>
+					<p><a href="#subscribe">Get the Picks</a></p>
+					<p><a href="#">Specials</a></p>
+					<p><a href="#">MY VEGAS LETTER</a></p>
+				</div>
+			</div>
+			<div style="clear:both"></div>
+			<div class="copyright">&copy;2015 Curtson Publications. All Rights Reserved.</div>
+		</footer>
 	</body>
 </html>
